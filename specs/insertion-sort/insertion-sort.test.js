e01780171cd57a77172b07ff9000a1c60a8280d4
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2023-04-14 13:09:24 -0700

    Brian Holt: Insertion sort

--- specs/insertion-sort/insertion-sort.test.js
+++ specs/insertion-sort/insertion-sort.test.js
@@ -33,6 +33,26 @@ function insertionSort(nums) {
 }
 
 
+
+// ----- BRIAN HOLT -----
+
+function insertionSort(nums) {
+  for (let i = 1; i < nums.length; i++) {
+    let numberToInsert = nums[i]; // the numberToInsert number we're looking to insert
+    let j; // the inner counter
+
+    // loop from the right to the left
+    for (j = i - 1; nums[j] > numberToInsert && j >= 0; j--) {
+      // move numbers to the right until we find where to insert
+      nums[j + 1] = nums[j];
+    }
+
+    // do the insertion
+    nums[j + 1] = numberToInsert;
+  }
+  return nums;
+}
+
 // unit tests
 // do not modify the below code
 test("insertion sort", function() {
